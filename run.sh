21b93fd9cd33d1ba65a9155f9aa9a9ae25f21f2e
Karl-Ludwig Besser <karl-ludwig.besser@liu.se>
2026-05-15 16:31:35 +0200

    Add N=30 OtA example
    Add the N=30 case of the over-the-air computation application example to
    the run script.

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 # This repository is accompanying the publication
 # "On the Distribution of a Two-Dimensional Random Walk with Restricted Angles"
-# (Karl-Ludwig Besser, Jul. 2025. arXiv:2507.15475).
+# (Karl-Ludwig Besser, IEEE Transactions on Signal Processing, 2026. arXiv:2507.15475).
 #
-# Copyright (C) 2025 Karl-Ludwig Besser
+# Copyright (C) 2025-2026 Karl-Ludwig Besser
 # License: MIT
 
 echo "Figures 2, 4, and 5: Distributions for N=2"
@@ -25,4 +25,5 @@ python simulation_many_steps.py -v -a 1.25 -N 5 --num_samples 10000000 --plot --
 python simulation_many_steps.py -v -a 1.25 -N 30 --num_samples 10000000 --plot --export
 
 echo "OtA example"
-python example_ota_computation.py -v --plot
+python example_ota_computation.py -v --plot -N 2
+python example_ota_computation.py -v --plot -N 30
